1baf839f21db5ee48e1adc9dce3a6929f544f44c
CY Lee <lcy511085@gmail.com>
2024-03-11 13:05:38 +0800

    Queue practice

--- Python/Data_Structure_and_Algorithm/Queue.py
+++ Python/Data_Structure_and_Algorithm/Queue.py
@@ -50,6 +50,35 @@ class Queue:
     def __init__(self):
         self.front = None
         self.rear = None
-    
+
+    def enqueue(self, item):
+        new_node = Node(item)
+        if self.rear is None:
+            self.front = self.rear = new_node
+        else:
+            self.rear.next = new_node
+            self.rear = new_node
+
+    def dequeue(self):
+        if self.is_empty():
+            print("Queue is empty")
+            return None
+        
+        item = self.front.data
+        self.front = self.front.next
+
+        if self.front is None:
+            self.rear = None
+        
+        return item
+
     def is_empty(self):
         return self.front is None
+    
+    def size(self):
+        count = 0
+        current = self.front
+        while current:
+            count += 1
+            current = current.next
+        return count
